b95380f5a1b98a04df201753d89e139fd45c5d45
cyamy <hi.cyamy@gmail.com>
2023-01-07 01:03:02 +0900

    chore: run format

--- a01/main.cpp
+++ a01/main.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-  int N;
-  cin >> N;
-  cout << N * N << endl;
-  return 0;
+    int N;
+    cin >> N;
+    cout << N * N << endl;
+    return 0;
 }
--- a02/main.cpp
+++ a02/main.cpp
@@ -2,20 +2,20 @@
 using namespace std;
 
 int main() {
-  int N, X, A[100];
-  cin >> N >> X;
+    int N, X, A[100];
+    cin >> N >> X;
 
-  for (int i = 0; i < N; i++) {
-    cin >> A[i];
-  }
-
-  for (int i = 0; i < N; i++) {
-    if (A[i] == X) {
-      cout << "Yes" << endl;
-      return 0;
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
     }
-  }
 
-  cout << "No" << endl;
-  return 0;
+    for (int i = 0; i < N; i++) {
+        if (A[i] == X) {
+            cout << "Yes" << endl;
+            return 0;
+        }
+    }
+
+    cout << "No" << endl;
+    return 0;
 }
