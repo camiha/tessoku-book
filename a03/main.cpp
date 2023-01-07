0e3f750f3efc32e28096f9b371c9803f70267942
cyamy <hi.cyamy@gmail.com>
2023-01-07 23:19:37 +0900

    2023/01/07 update

---
+++ a03/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, K, P[100], Q[100];
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        cin >> P[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        cin >> Q[i];
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (P[i] + Q[j] == K) {
+                cout << "Yes" << endl;
+                return 0;
+            }
+        }
+    }
+
+    cout << "No" << endl;
+    return 0;
+}
